64ec236cc0481729ef5353d9804748e00434511e
Meghana <meghanam2744@gmail.com>
2026-05-29 13:49:58 +0530

    updated API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://task-manager-api-p9rp.onrender.com/"
 });
 
 export default API;
